d81242f77115d61f2ac690d8e25f0c8a4d765a27
LEO100a0 <leo680961@gmail.com>
2026-03-20 16:45:14 -0500

    Update temperatura_controller.py

--- app/controllers/temperatura_controller.py
+++ app/controllers/temperatura_controller.py
@@ -32,11 +32,3 @@ def obtener_datos():
 
     return {"datos": datos}
     
-n = len(datos)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if datos[j]["valor"] > datos[j + 1]["valor"]:
-                # Intercambiar
-                datos[j], datos[j + 1] = datos[j + 1], datos[j]
-
-    return {"datos": datos}
